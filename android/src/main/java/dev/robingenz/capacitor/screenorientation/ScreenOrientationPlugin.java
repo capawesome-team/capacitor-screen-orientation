18ed5b021a2a5f19ede6027f8fbc0ef3cccdb0fb
Robin Genz <mail@robingenz.dev>
2021-05-01 17:37:52 +0200

    feat: add `android` support (#2)

--- android/src/main/java/dev/robingenz/capacitor/screenorientation/ScreenOrientationPlugin.java
+++ android/src/main/java/dev/robingenz/capacitor/screenorientation/ScreenOrientationPlugin.java
@@ -9,14 +9,31 @@ import com.getcapacitor.annotation.CapacitorPlugin;
 @CapacitorPlugin(name = "ScreenOrientation")
 public class ScreenOrientationPlugin extends Plugin {
 
-    private ScreenOrientation implementation = new ScreenOrientation();
+    private ScreenOrientation implementation;
+
+    @Override
+    public void load() {
+        implementation = new ScreenOrientation(getBridge());
+    }
 
     @PluginMethod
-    public void echo(PluginCall call) {
-        String value = call.getString("value");
+    public void lock(PluginCall call) {
+        String orientationType = call.getString("type");
+        implementation.lock(orientationType);
+        call.resolve();
+    }
 
+    @PluginMethod
+    public void unlock(PluginCall call) {
+        implementation.unlock();
+        call.resolve();
+    }
+
+    @PluginMethod
+    public void getCurrentOrientation(PluginCall call) {
         JSObject ret = new JSObject();
-        ret.put("value", implementation.echo(value));
+        String orientationType = implementation.getCurrentOrientationType();
+        ret.put("type", orientationType);
         call.resolve(ret);
     }
 }
