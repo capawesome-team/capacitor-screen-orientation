2a4403bd6784252b7f5dc2f293b2e88705777692
Robin Genz <mail@robingenz.dev>
2021-05-01 13:29:49 +0200

    feat: add `web` support (#1)

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,3 +1,55 @@
 export interface ScreenOrientationPlugin {
-  echo(options: { value: string }): Promise<{ value: string }>;
+  /**
+   * Locks the device orientation.
+   */
+  lock(options: LockOptions): Promise<void>;
+  /**
+   * Unlocks the device orientation.
+   */
+  unlock(): Promise<void>;
+  /**
+   * Gets the current device orientation type.
+   */
+  getCurrentOrientation(): Promise<GetCurrentOrientationResult>;
+}
+
+export interface LockOptions {
+  /**
+   * The orientation lock type.
+   */
+  type: OrientationType;
+}
+
+export interface GetCurrentOrientationResult {
+  /**
+   * The orientation lock type.
+   */
+  type: OrientationType;
+}
+
+export enum OrientationType {
+  /**
+   * The orientation is either landscape-primary or landscape-secondary.
+   */
+  LANDSCAPE = 'landscape',
+  /**
+   * The orientation is in the primary landscape mode.
+   */
+  LANDSCAPE_PRIMARY = 'landscape-primary',
+  /**
+   * The orientation is in the secondary landscape mode.
+   */
+  LANDSCAPE_SECONDARY = 'landscape-secondary',
+  /**
+   * The orientation is either portrait-primary or portrait-secondary.
+   */
+  PORTRAIT = 'portrait',
+  /**
+   * The orientation is in the primary portrait mode.
+   */
+  PORTRAIT_PRIMARY = 'portrait-primary',
+  /**
+   * The orientation is in the secondary portrait mode.
+   */
+  PORTRAIT_SECONDARY = 'portrait-secondary',
 }
