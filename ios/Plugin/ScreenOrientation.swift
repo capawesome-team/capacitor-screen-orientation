7b4ad35a595f885c2ad2ad19d72da65ab579cf52
Robin Genz <mail@robingenz.dev>
2021-05-10 21:56:27 +0200

    wip

--- ios/Plugin/ScreenOrientation.swift
+++ ios/Plugin/ScreenOrientation.swift
@@ -2,16 +2,56 @@ import Foundation
 
 @objc public class ScreenOrientation: NSObject {
     @objc public func lock(_ orientationType: String) {
-        // TODO
+        DispatchQueue.main.async {
+            let orientationValue = self.convertOrientationTypeToValue(orientationType)
+            UIDevice.current.setValue(orientationValue, forKey: "orientation")
+            UINavigationController.attemptRotationToDeviceOrientation()
+        }
     }
     
     @objc public func unlock() {
-        // TODO
+        DispatchQueue.main.async {
+            UIDevice.current.setValue(0, forKey: "orientation")
+        }
     }
     
     @objc public func getCurrentOrientationType() -> String {
-        // TODO
-        let orientationType = ""
+        let orientationValue = UIDevice.current.orientation.rawValue
+        let orientationType = self.convertOrientationValueToType(orientationValue)
         return orientationType
+    }
+    
+    @objc private func convertOrientationTypeToValue(_ orientationType: String) -> Int {
+        switch orientationType {
+        case "landscape":
+            return UIInterfaceOrientation.landscapeRight.rawValue
+        case "landscape-primary":
+            return UIInterfaceOrientation.landscapeLeft.rawValue
+        case "landscape-secondary":
+            return UIInterfaceOrientation.landscapeRight.rawValue
+        case "potrait":
+            return UIInterfaceOrientation.portrait.rawValue
+        case "potrait-primary":
+            return UIInterfaceOrientation.portrait.rawValue
+        case "potrait-secondary":
+            return UIInterfaceOrientation.portraitUpsideDown.rawValue
+        default:
+            return UIInterfaceOrientation.unknown.rawValue
+        }
+    }
+    
+    @objc private func convertOrientationValueToType(_ orientationValue: Int) -> String {
+        switch orientationValue {
+        case UIInterfaceOrientation.landscapeLeft.rawValue:
+            return "landscape-primary"
+        case UIInterfaceOrientation.landscapeRight.rawValue:
+            return "landscape-secondary"
+        case UIInterfaceOrientation.portrait.rawValue:
+            return "potrait-primary"
+        case UIInterfaceOrientation.portraitUpsideDown.rawValue:
+            return "potrait-secondary"
+        default:
+            return "potrait-primary"
+        }
     }
 }
